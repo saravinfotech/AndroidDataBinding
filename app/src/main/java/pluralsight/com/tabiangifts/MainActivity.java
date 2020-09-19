1f01bd159bf99f0008483c94c38f4db5e06cd184
Saravanan K <saravinfotech@gmail.com>
2020-09-19 21:31:37 +0100

    Binding Views

--- app/src/main/java/pluralsight/com/tabiangifts/MainActivity.java
+++ app/src/main/java/pluralsight/com/tabiangifts/MainActivity.java
@@ -1,14 +1,20 @@
 package pluralsight.com.tabiangifts;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import pluralsight.com.tabiangifts.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
     }
 }
